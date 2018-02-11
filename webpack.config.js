a71c37a52c68bfac92971f2781dee38779c689c0
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-02-12 00:44:16 +0400

    add new entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var path = require('path');
 
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: {
+        main: ['./src/index.js'],
+        dev: ['./test/index.js']
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, './dist/')
     },
     module: {
