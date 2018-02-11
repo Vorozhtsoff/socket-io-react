87486f8de1d335e43ff46edbc30b49346b7adbe2
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-02-11 23:36:53 +0400

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist/')
+    },
+    module: {
+        rules: [
+            { test: /\.jsx?$/, use: 'babel-loader' }
+        ]
+    }
+};
