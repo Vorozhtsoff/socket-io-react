24e463fd1a31ccd7176aa6d40672868396b7ba95
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-02-22 02:37:39 +0400

    add middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,33 @@
-const ReduxSocketMiddleware = store => next => (action) => {
+import io from 'socket.io-client';
+import * as actionTypes from './types';
 
-    return next(action);
-};
+const socketEmit = socket =>
+    (eventName, payload) => socket.emit(eventName, payload);
 
-export default ReduxSocketMiddleware;
+
+const reduxSocketMiddleware = socket => (store) => {
+    conssole.log('store', store);
+    return next => (action) => {
+        switch(action.type) {
+            case actionTypes.SOCKET_EMIT: {
+                socketEmit(socket)(action.eventName, action.payload);
+                break;
+            }
+            case action.SOCKET_CONNECT: {
+                socket.connect
+            }
+        }
+
+        return next(action);
+    };
+}
+
+export default function createSocketMiddleware(endpoint, config = {}, listeners) {
+    const socket = io(endpoint = '/', { autoconnect: false, ...config });
+
+    socket.on('connection', () => {
+        listeners(socket);
+    });
+
+    return reduxSocketMiddleware(socket);
+}
