1bc0408dc1b67f7b57b73442cb3ee53ac34a734f
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-02-22 02:41:49 +0400

    add test application

--- test/app.js
+++ test/app.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 
 import configureStore from './configure-store';
+import { socketConnect } from '../src';
 
 
 const store = configureStore({});
+
+const mapActionToDispatch = {
+    socketConnect
+};
+
+@connect(null, mapActionToDispatch)
+class Children extends React.Component {
+    componentDidMount() {
+        this.props.socketConnect();
+    }
+
+    render() {
+        return null;
+    }
+}
+
 
 export default class App extends React.Component {
     render() {
         return (
             <Provider store={ store }>
-                <div>Hello world</div>
+                <Children />
             </Provider>
         );
     }
--- test/configure-store.js
+++ test/configure-store.js
@@ -1,21 +1,25 @@
 /* eslint-disable global-require */
-
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { createSocketMiddleware } from '../src';
 
 import reducers from './reducers';
 
 export default function configureStore(initState = {}) {
 
-        const composeEnhancers =
-            typeof window === 'object' &&
-            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? // eslint-disable-line no-underscore-dangle
-                window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;  // eslint-disable-line no-underscore-dangle
+    const composeEnhancers =
+        typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? // eslint-disable-line no-underscore-dangle
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;  // eslint-disable-line no-underscore-dangle
 
-        const store = createStore(
-            reducers,
-            initState,
-            composeEnhancers()
-        );
+    const store = createStore(
+        reducers,
+        initState,
+        composeEnhancers(),
+        applyMiddleware(
+            createSocketMiddleware('//localhost:3000/', null, (...args) => console.log(args))
+        )
 
-        return store;
+    );
+
+    return store;
 }
