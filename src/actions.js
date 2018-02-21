3b9ed5879b8adf1ee3050b78b5650017475d703b
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-02-22 02:36:14 +0400

    add actions

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,4 @@
-import { SOCKET_EMIT } from './types';
+import { SOCKET_EMIT, SOCKET_CONNECT } from './types';
 
-export const emit = (eventName, payload) => ({ type: SOCKET_EMIT, eventName, payload });
+export const socketEmit = (eventName, payload) => ({ type: SOCKET_EMIT, eventName, payload });
+export const socketConnect = () => ({ type: SOCKET_CONNECT });
