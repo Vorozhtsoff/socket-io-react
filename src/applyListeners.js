85a12e190262fdf9de88b48a91e35d7dcee2214a
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-03-11 04:51:51 +0400

    applyListeners check values types

--- src/applyListeners.js
+++ src/applyListeners.js
@@ -1,5 +1,19 @@
+import { isObject, isFunc } from './helpers';
+import { warn } from './logger';
+
+
 const applyListeners = listeners => (socket, store) => () => {
-    Object.entries(listeners).forEach(([listenerName, fn]) => socket.on(listenerName, fn(store)));
+    if (!isObject(listeners)) {
+        return warn('listeners must be a object, when key is listener name and value is function');
+    }
+
+    return Object.entries(listeners).forEach(([listenerName, fn]) => {
+        if (!isFunc(fn)) {
+            return warn(`${listenerName} value is not a faction`);
+        }
+
+        return socket.on(listenerName, fn(store));
+    });
 };
 
 export default applyListeners;
