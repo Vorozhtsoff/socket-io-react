bb8c7cbbb1d69fa4e3453dc0481a4351978f78e2
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-03-11 06:22:36 +0400

    setup example socket handler with dispatching action

--- test-app/listeners.js
+++ test-app/listeners.js
@@ -1,5 +1,7 @@
 import { applyListeners } from '../src';
 
+const recieveResponse = payload => ({ type: 'RECIEVE_RESPONSE', payload });
+
 export default applyListeners({
-    sendResponse: (data, store) => console.log('responsed data', data, store)
+    sendResponse: (data, store) => console.log('responsed data', data) || store.dispatch(recieveResponse(data))
 });
