4eddb3a71be341ce92882cc005ee5bef40af04ca
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-03-11 06:06:01 +0400

    add export helpers functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { socketEmit, socketConnect } from './actions';
 export { default as createSocketMiddleware } from './middleware';
 export { default as applyListeners } from './applyListeners';
+export * from './helpers';
